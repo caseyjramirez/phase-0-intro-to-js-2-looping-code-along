09f2d9b694fb15ed252ec493f7f2f29c2962a1b5
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-11 17:29:40 -0500

    commit tree 193853f7664d3c2ec55992d25799c52fb7130cb8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(myList) {
+    const thankYouMessages = []
+    for(let name of myList) {
+        thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    for(let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
